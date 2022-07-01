86c6f97f9a136390894191901241dfb608eb83a5
LeadConsult <leadconsultnig@gmail.com>
2022-06-30 17:41:21 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  *  * main - Entry point
- *   * Description: 'task 4 description'
+ *   * Description: 'task 5 description'
  *    * Return: Always 0 (Success)
- *     */
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

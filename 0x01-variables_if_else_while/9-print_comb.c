c60d7723eb72045ee962ba60dfb4a7f82510d7e3
LeadConsult <leadconsultnig@gmail.com>
2022-07-01 20:52:33 -0700

    taskadv1

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - program that prints all possible combinations of single-digit numbers.
+ * main - program that prints all possible 
+ * combinations of single-digit numbers.
  * Return: Always 0 (Success)
  */
 int main(void)

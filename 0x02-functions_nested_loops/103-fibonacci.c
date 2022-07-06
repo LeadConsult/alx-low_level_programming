9ecfe447ef506d265b9030033c2137415996ccc5
LeadConsult <leadconsultnig@gmail.com>
2022-07-05 19:29:48 -0700

    task15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * * main - main entry
+ * * Description:  program that finds & prints the sum of the even-valued terms
+ * * Return: 0
+*/
+int main(void)
+{
+	int x = 0, y = 1, next = 0;
+	int sum = 0;
+
+	while (next < 4000000)
+	{
+		next = x + y;
+		x = y;
+		y = next;
+
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
+}
+putchar('\n');
+
+return (0);
+}
+

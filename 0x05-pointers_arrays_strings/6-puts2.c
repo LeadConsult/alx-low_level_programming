178bb7fcaec1fee5a125135f4c972a586732eec9
LeadConsult <leadconsultnig@gmail.com>
2022-07-12 17:46:17 -0700

    Task6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * * puts2 -  function that prints every other character of a string,
- *   starting with the first character, followed by a new line.
+ * * puts2 -  function that prints every other character of a string, starting
+ *   with the first character, followed by a new line.
  * * @str: input string.
  * * Return: void
 */
@@ -21,9 +21,4 @@ void puts2(char *str)
 			_putchar(str[count]);
 		count++;
 	}
-}
-*(s + i) = *(str + j);
-j++;
-i--;
-}
 }

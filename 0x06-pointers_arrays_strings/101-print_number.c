7ed5b644d7f8902373534c79083bca1f1342a6c2
LeadConsult <leadconsultnig@gmail.com>
2022-07-14 20:06:28 -0700

    Task9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * * print_number - a function that prints an integer.
+ * * @n: integer to be printed;
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	} else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
+}

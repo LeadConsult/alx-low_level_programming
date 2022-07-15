5c16a7f8aa40cfb0f6802ba8aeddc4b57d937f1c
LeadConsult <leadconsultnig@gmail.com>
2022-07-14 20:47:42 -0700

    Task12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,7 +5,7 @@
  * * print_buffer - a function that prints a buffer.
  * * @b: buffer.
  * * @size: size of the buffer.
- * * Return: void.
+ * * Return: always 0.
  */
 void print_buffer(char *b, int size)
 {

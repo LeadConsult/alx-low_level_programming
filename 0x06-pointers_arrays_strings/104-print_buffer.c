2730f45baafcfab854ac8d031d747f83249e215f
LeadConsult <leadconsultnig@gmail.com>
2022-07-14 20:46:10 -0700

    Task12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * * print_buffer - a function that prints a buffer.
+ * * @b: buffer.
+ * * @size: size of the buffer.
+ * * Return: void.
+ */
+void print_buffer(char *b, int size)
+{
+	int x, y, z;
+
+	if (size <= 0)
+		printf("\n");
+	else
+	{
+		for (x = 0; x < size; x += 10)
+		{
+			printf("%.8x:", x);
+			for (y = x; y < x + 10; y++)
+			{
+				if (y % 2 == 0)
+					printf(" ");
+				if (y < size)
+					printf("%.2x", *(b + y));
+				else
+					printf("  ");
+			}
+			printf(" ");
+			for (z = x; z < x + 10; z++)
+			{
+				if (z >= size)
+					break;
+				if (*(b + z) < 32 || *(b + z) > 126)
+					printf("%c", '.');
+				else
+					printf("%c", *(b + z));
+			}
+			printf("\n");
+		}
+	}
+}

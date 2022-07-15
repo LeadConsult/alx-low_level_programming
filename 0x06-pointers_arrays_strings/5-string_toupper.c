62c5de088e2aa522b81c92281e7631c32737004f
LeadConsult <leadconsultnig@gmail.com>
2022-07-14 21:02:17 -0700

    Task06

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,26 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * * * string_toupper - a function that changes all
- * * lowercase letters of a string to uppercase
- * * * @p: The string that will be changed
- * * *
- * * * Return: string
- */
-
-char *string_toupper(char *p)
+int main(void)
 {
-	int a = 0;
+	int n;
+	int a[5];
+	int *p;
 
-	while (p[a])
-	{
-		if (p[a] >= 97 && p[a] <= 122)
-		{
-			p[a] -= 32;
-		}
-
-		a++;
-	}
-
-	return (p);
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	p[5] = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }

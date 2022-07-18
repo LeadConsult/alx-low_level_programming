3ce210cc6c2b8bcc52a813f77e013a52fa7848ff
LeadConsult <leadconsultnig@gmail.com>
2022-07-18 15:20:15 -0700

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * *_memset - a function that fills memory with a constant byte
- * *@s: pointer 
+ * *@s: pointer
  * *@b: constant byte
  * *@n: bytes of the memory
  * *Return: dest

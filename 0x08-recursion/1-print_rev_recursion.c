c7951a65848214a2fd5d68a8fdd271dd2deaedfa
LeadConsult <leadconsultnig@gmail.com>
2022-07-20 14:26:37 -0700

    task2

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,9 +2,10 @@
 
 /**
  * * _puts_recursion - a function that prints a string in reverse.
- * * @s: the string to print
- * * Return: void
+ * * @s: the string to print.
+ * Return: Always 0.
 */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')

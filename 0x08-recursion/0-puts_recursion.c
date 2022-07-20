d656d32adb670770b85dd28c023bfab1050395e7
LeadConsult <leadconsultnig@gmail.com>
2022-07-20 14:06:53 -0700

    task0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * * _puts_recursion - a function that prints a string, followed by a new line
+ * * @s: the string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is readme file for recursion task on alx dated 20 july 2022

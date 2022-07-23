7c96168b9448781fb71657da8a6340aa9920b27e
LeadConsult <leadconsultnig@gmail.com>
2022-07-22 17:37:49 -0700

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * * main - function that prints its name, followed by a new line.
+ * * @argc: The number of command line arguments, The size of the argv array
+ * * @argv: array containing program command line arguments,array of size argc
+ * * Return: 0 - success.
+ */
+
+int main(int argc, char  *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+}

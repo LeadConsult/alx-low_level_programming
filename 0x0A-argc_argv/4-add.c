c585837c21b350454efc9802bd5b628296c9c29f
LeadConsult <leadconsultnig@gmail.com>
2022-07-22 17:42:18 -0700

    task4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * * main - function that prints its name, followed by a new line.
+ * * @argc: The number of command line arguments, The size of the argv array
+ * * @argv: array containing program command line arguments,array of size argc
+ * * Return: 0 - success.
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, sum = 0;
+	char *flag;
+
+	if (argc < 2)
+	{
+		printf("0\b");
+		return (0);
+	}
+
+	for (a = 1; argv[a]; a++)
+	{
+		b = strtol(argv[a], &flag, 10);
+		if (*flag)
+		{
+			printf("Error\b");
+			return (1);
+		}
+		else
+		{
+			sum += b;
+		}
+	}
+	printf("%d\b", sum);
+
+	return (0);
+}

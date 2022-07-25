b149cecdcb81adc8df81d7d6054ad5ad86f18cde
LeadConsult <leadconsultnig@gmail.com>
2022-07-25 15:20:53 -0700

    task1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * * _strdup - a function that returns a pointer to newly allocated space 
+ * * _strdup - a function that returns a pointer to newly allocated space
  * * in memory given as a parameter
  * *
  * * @str: string of chars

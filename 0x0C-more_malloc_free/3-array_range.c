f8acc6160ff6b4fd46bf0287cf0002d57299d328
LeadConsult <leadconsultnig@gmail.com>
2022-07-28 16:51:56 -0700

    task3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -21,7 +21,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	ar = malloc(sizeof(*arr) * ((max - min) + 1));
+	arr = malloc(sizeof(*arr) * ((max - min) + 1));
 
 	if (arr == NULL)
 		return (NULL);

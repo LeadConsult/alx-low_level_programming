c42389ef923ccba5ad86c0c9036841381d49db35
LeadConsult <leadconsultnig@gmail.com>
2022-07-28 16:36:11 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,21 +2,22 @@
 #include <stdlib.h>
 
 /**
- *  string_nconcat - a function that concatenates two strings
+ * * string_nconcat - a function that concatenates two strings
  *
- *   * @s1: first string
+ * * @s1: first string
  *
- *    * @s2: second string
+ * * @s2: second string
  *
- *     * @n: number of bytes from s2 to concatenate to s1
+ * * @n: number of bytes from s2 to concatenate to s1
  *
- *      *
+ * *
  *
- *       * Return: pointer to the allocated memory.
+ * * Return: pointer to the allocated memory.
  *
- *        * if malloc fails, status value is equal to 98.
+ * * if malloc fails, status value is equal to 98.
  *
  */
+
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
@@ -25,38 +26,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s1 && s1[len1])
 		len1++;
-
 	while (s2 && s2[len2])
-		len2++
+		len2++;
 
 	if (n < len2)
-
 		s = malloc(sizeof(char) * (len1 + n + 1));
-
 	else
-
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-
-
 	if (!s)
-
 		return (NULL);
 
 	while (i < len1)
-
 	{
-
 		s[i] = s1[i];
-
 		i++;
-
 	}
 
 	while (n < len2 && i < (len1 + n))
 		s[i++] = s2[j++];
+
 	while (n >= len2 && i < (len1 + len2))
 		s[i++] = s2[j++];
+
 	s[i] = '\0';
+
+
 	return (s);
+
 }

dc469e2eefadfd595a18c8bdb6582421b255d7e2
LeadConsult <leadconsultnig@gmail.com>
2022-08-03 18:47:04 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * * print_name - a function that prints a name.
+ * * @name: name of the person
+ * * @f: function pointer
+ * * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

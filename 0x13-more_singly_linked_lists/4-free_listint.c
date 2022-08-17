aab474c5e4ae61abbb2bba1a63376f5ee61485f1
LeadConsult <leadconsultnig@gmail.com>
2022-08-16 17:26:07 -0700

    task4

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,17 +3,14 @@
  * * free_listint - Frees a list of nodes
  * * @head: Pointer to the first element of the list
  */
+
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
-		return;
-	while (head->next != NULL)
+	while ((temp = head) != NULL)
 	{
-		tmp = head->next;
-		free(head);
-		head = temp;
+		head = head->next;
+		free(temp);
 	}
-	free(head);
 }
